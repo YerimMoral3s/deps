7e048cc0d1c84fdedde9946e0a02ce78b20296d4
YerimMoral3s <94862683+YerimMoral3s@users.noreply.github.com>
2025-02-08 14:50:09 -0600

    fix

--- src/api/axios/index.tsx
+++ src/api/axios/index.tsx
@@ -12,17 +12,6 @@ const axiosInstance = axios.create({
 });
 
 axiosInstance.interceptors.request.use(
-  (config) => {
-    const accessToken = localStorage.getItem("access_token");
-    if (accessToken) {
-      config.headers.Authorization = `Bearer ${accessToken}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
-
-axiosInstance.interceptors.request.use(
   async (config) => {
     const cred = await encryptWithJWT();
     config.headers["x-api-key"] = cred;
