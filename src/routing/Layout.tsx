5727eb51b147ef0c833089f21b2ac74fda1c5c62
YerimMoral3s <94862683+YerimMoral3s@users.noreply.github.com>
2025-02-08 21:19:34 -0600

    add padding if responsive

--- src/routing/Layout.tsx
+++ src/routing/Layout.tsx
@@ -3,12 +3,22 @@ import { Outlet } from "react-router-dom";
 import { Suspense } from "react";
 import LoaderView from "../components/LoaderView";
 import { Navbar } from "../components";
+import styled from "styled-components";
+
+const StyledLayout = styled.div`
+  @media (max-width: 768px) {
+    padding-bottom: 60px; /* ✅ Adds space for the fixed bottom Navbar */
+  }
+`;
 
 const ProtectedLayout = () => {
   return (
     <Suspense fallback={<LoaderView />}>
-      <Navbar />
-      <Outlet />
+      <StyledLayout>
+        <Navbar />
+
+        <Outlet />
+      </StyledLayout>
     </Suspense>
   );
 };
